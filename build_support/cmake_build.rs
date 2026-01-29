703b418712067470ca1714e042efb0e3a3a68da1
yhw2003 <haowu.yang@stepai.cn>
2026-01-29 20:02:58 +0800

    fix: 修复交叉编译

--- build_support/cmake_build.rs
+++ build_support/cmake_build.rs
@@ -1,13 +1,10 @@
-use std::{env, path::PathBuf};
+use std::path::PathBuf;
 
 pub fn build_with_cmake(target_triple: &str) -> PathBuf {
   let mut cfg = cmake::Config::new("pcap-core");
 
   cfg.define("CMAKE_EXPORT_COMPILE_COMMANDS", "ON");
   cfg.define("INSTALL_VCPKG_PCAP_STATIC", "ON");
-
-  let toolchain_file = vcpkg_toolchain_file();
-  cfg.define("CMAKE_TOOLCHAIN_FILE", toolchain_file);
 
   if target_triple == "aarch64-unknown-linux-gnu" {
     cfg.define("VCPKG_TARGET_TRIPLET", "arm64-linux");
@@ -18,12 +15,4 @@ pub fn build_with_cmake(target_triple: &str) -> PathBuf {
   }
 
   cfg.build()
-}
-
-fn vcpkg_toolchain_file() -> PathBuf {
-  let vcpkg_root = env::var("VCPKG_ROOT").expect("VCPKG_ROOT is not set");
-  PathBuf::from(vcpkg_root)
-    .join("scripts")
-    .join("buildsystems")
-    .join("vcpkg.cmake")
 }
